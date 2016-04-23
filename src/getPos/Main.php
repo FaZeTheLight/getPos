df250da590ed04346974d4258d95b3d3eb573a2a
SavionLegendZzz <saviondeavault@gmail.com>
2016-04-23 17:35:03 -0400

    Update Main.php

--- src/getPos/Main.php
+++ src/getPos/Main.php
@@ -10,23 +10,23 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\level\Position;
 use pocketmine\utils\TextFormat as color;
 class Main extends PluginBase implements Listener{
+	
 public function onEnable(){
 $this->getServer()->getLogger()->info("[getPos]v1.0 by BoyIsDino Enabled!");
 $this->getServer()->getPluginManager()->registerEvents($this,$this);
 }
 
 public function onCommand(CommandSender $sender, Command $command, $label, array $args) {
-        switch ($command){
+        switch(strtolower($command->getName())){
             case 'getpos':
 			$x = $p->getX();
 			$y = $p->gety();
                         $z = $p->getZ();
-				$sender->sendMessage("Your current coordinates are; ".color::RED.$x[$p->getx()]."\n".color::RESET."X=".color::BlUE.$y[$p->gety()]."\n".color::RESET."Y=".color::GREEN.$z[$p->getZ()]."\n".color::RESET."Z=");
+			$sender->sendMessage("Your coords are: ".$x.",".$y.",".$z."!");
 		}
 }
-public function Disable(){
+public function onDisable(){
 $this->getServer()->getLogger()->info("[getPos]v1.0 by BoyIsDino Disabled!");
-$this->getServer()->getPluginManager()->registerEvents($this,$this);
 }
 
 }
